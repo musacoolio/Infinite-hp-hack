60d2787977f678e5403761d3eada98f40bb55f4b
musacoolio <149318974+musacoolio@users.noreply.github.com>
2025-09-06 18:59:12 +0300

    Update the_hack.lua

--- the_hack.lua
+++ the_hack.lua
@@ -1,4 +1,4 @@
-while true:
+while true do
     local skibidi = game:WaitForChild("Players")
     local thekoolesthecker = skibidi:WaitForChild("musahero20")
     local thekoolestheckerschar = thekoolesthecker.Character or thekoolesthecker.CharacterAdded:Wait()
