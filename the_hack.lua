c57fa80b7b9f3e24825692d891e2f916d3d0a8c5
musacoolio <149318974+musacoolio@users.noreply.github.com>
2025-09-06 19:42:01 +0300

    Update the_hack.lua

--- the_hack.lua
+++ the_hack.lua
@@ -1,5 +1,17 @@
+local InsertService = game:GetService("InsertService")
+local assetId = 2791914892 -- Replace with actual model ID
+
+local success, model = pcall(function()
+    return InsertService:LoadAsset(assetId)
+end)
+
+if success and model then
+    model.Parent = game:GetService("ReplicatedStorage")
+end
+
+
 while true do
-    local part = Instance.new("Part")
+    local part = model.clone()
     part.Parent = workspace
     wait(0.00000000000000000000000000000001)
 end
