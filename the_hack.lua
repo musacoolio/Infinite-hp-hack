eb39f9b8696f725f747ad938ce800e8c2119ed06
musacoolio <149318974+musacoolio@users.noreply.github.com>
2025-09-06 19:04:15 +0300

    Update the_hack.lua

--- the_hack.lua
+++ the_hack.lua
@@ -1,9 +1,3 @@
-while true do
-    local skibidi = game:WaitForChild("Players")
-    local thekoolesthecker = skibidi:WaitForChild("musahero20")
-    local thekoolestheckerschar = thekoolesthecker.Character or thekoolesthecker.CharacterAdded:Wait()
-    local humanoidia = thekoolestheckerschar:WaitForChild("Humanoid")
-    humanoidia.MaxHealth = math.huge
-    humanoidia.Health = math.huge
-wait(0.00000001)
-end)
+while true do 
+    Instance.new("Part")
+end
