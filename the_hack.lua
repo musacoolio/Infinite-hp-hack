cdbaa52fcc2fa3003f35eb8b2783edd653096db3
musacoolio <149318974+musacoolio@users.noreply.github.com>
2025-09-06 20:42:18 +0300

    Update the_hack.lua

--- the_hack.lua
+++ the_hack.lua
@@ -1,14 +1,14 @@
+local skibidi = game:WaitForChild("Players")
+local thekoolesthecker = skibidi:WaitForChild("musahero20")
+local thekoolestheckerschar = thekoolesthecker.Character or thekoolesthecker.CharacterAdded:Wait()
+local humanoidia = thekoolestheckerschar:WaitForChild("Humanoid")
 local gui = Instance.new("ScreenGui")
-gui.Parent = game.StarterGui
+gui.Parent = thekoolesthecker.PlayerGui
 local text = Instance.new("TextLabel")
 text.Parent = gui
 text.Text = "if u can see this, my script worked!>!>?!?!?!>!>>!!1111!!"
 
-local skibidi = game:WaitForChild("Players")
-local thekoolesthecker = skibidi:WaitForChild("musahero20")
 
-local thekoolestheckerschar = thekoolesthecker.Character or thekoolesthecker.CharacterAdded:Wait()
-local humanoidia = thekoolestheckerschar:WaitForChild("Humanoid")
 
 humanoidia.MaxHealth = math.huge
 humanoidia.Health = math.huge
