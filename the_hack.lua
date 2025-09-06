2ee221623e53793b11f68cb50a4fd1a3f8880725
musacoolio <149318974+musacoolio@users.noreply.github.com>
2025-09-06 18:45:18 +0300

    Update the_hack.lua

--- the_hack.lua
+++ the_hack.lua
@@ -5,7 +5,7 @@ local thekoolesthecker = skibidi:WaitForChild("musahero20")
 local thekoolestheckerschar = thekoolesthecker.Character or thekoolesthecker.CharacterAdded:Wait()
 local humanoidia = thekoolestheckerschar:WaitForChild("Humanoid")
 
-humanoidia.MaxHealth = 100
-humanoidia.Health = 100
+humanoidia.MaxHealth = 10000000000000000000000000000000000000000000000000000000000000000000000000000
+humanoidia.Health = 1000000000000000000000000000000000000000000000000000000000000000000
   wait(0.1)
 end
