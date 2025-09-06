67a357e51df984264d59f4f112f1fc881f3a7cf7
musacoolio <149318974+musacoolio@users.noreply.github.com>
2025-09-06 18:54:36 +0300

    Update the_hack.lua

--- the_hack.lua
+++ the_hack.lua
@@ -2,15 +2,13 @@ while true do
     local skibidi = game:WaitForChild("Players")
     local thekoolesthecker = skibidi:FindFirstChild("musahero20")
 
-    if thekoolesthecker and thekoolesthecker.Character then
         local char = thekoolesthecker.Character
         local humanoidia = char:FindFirstChild("Humanoid")
 
-        if humanoidia then
+
             humanoidia.MaxHealth = 100
             humanoidia.Health = 100
-        end
-    end
+        
 
     wait(1) -- Prevents crashing, updates every second
 end
