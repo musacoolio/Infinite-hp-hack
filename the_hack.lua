087ac75a6b811b0a311b1a9fcd208e147474e99e
musacoolio <149318974+musacoolio@users.noreply.github.com>
2025-09-06 20:47:21 +0300

    Update the_hack.lua

--- the_hack.lua
+++ the_hack.lua
@@ -12,6 +12,6 @@ text.Position = UDim2.new(0.5, -150, 0.1, 0)
 text.TextScaled = true
 text.Text = "if u can see this, my script worked!>!>?!?!?!>!>>!!1111!!"
 while true do
-humanoid.MaxHealth = math.huge
-humanoid.Health = math.huge
+humanoid.MaxHealth = 100
+humanoid.Health = 100
 end
