c84fbd30e6bc43b56b382152bf15ed18b6ebcfa4
musacoolio <149318974+musacoolio@users.noreply.github.com>
2025-09-07 18:17:39 +0300

    Update the_hack.lua

--- the_hack.lua
+++ the_hack.lua
@@ -1,1 +1,10 @@
-walkTree(workspace)
+local function walkTree(root)
+    print(root.Name)
+    for _, child in ipairs(root:GetChildren()) do
+        walkTree(child)
+    end
+end
+
+walkTree(workspace) -- ✅ This must come after the function
+
+
