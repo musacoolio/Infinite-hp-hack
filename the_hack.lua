502bc8bd085399d6690573f035e1115d98d2f3cb
musacoolio <149318974+musacoolio@users.noreply.github.com>
2025-09-07 14:58:11 +0300

    Update the_hack.lua

--- the_hack.lua
+++ the_hack.lua
@@ -5,13 +5,13 @@ local humanoid = char:WaitForChild("Humanoid")
 local gui = Instance.new("ScreenGui")
 gui.Parent = player:WaitForChild("PlayerGui")
 
-local text = Instance.new("TextLabel")
+local text = Instance.new("TextButton")
 text.Parent = gui
 text.Size = UDim2.new(0, 300, 0, 50)
-text.Position = UDim2.new(0.5, -150, 0.1, 0)
+text.Position = UDim2.new(0, 0, 0, 0)
 text.TextScaled = true
-text.Text = "if u can see this, my script worked!>!>?!?!?!>!>>!!1111!!"
-while true do
+text.Text = "Heal"
+text.MouseButton1Click:Connect(function()
 humanoid.MaxHealth = 100
 humanoid.Health = 100
-end
+  end)
