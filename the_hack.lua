e66e6cc408e96291eb3f166c50594905e3978b63
musacoolio <149318974+musacoolio@users.noreply.github.com>
2025-10-03 10:59:50 +0300

    Update the_hack.lua

--- the_hack.lua
+++ the_hack.lua
@@ -12,8 +12,29 @@ billboardGui.Parent = head
 
 local textLabel = Instance.new("TextLabel")
 textLabel.Size = UDim2.new(1, 0, 1, 0)
-textLabel.BackgroundTransparency = 1
+textLabel.BackgroundTransparency = 0
 textLabel.Text = "sTOP lAUGHING IDIOT"
 textLabel.TextColor3 = Color3.new(1, 1, 1)
 textLabel.TextScaled = true
+textLabel.TextTransparency = 1
 textLabel.Parent = billboardGui
+
+local editlabel = Instance.new("TextBox")
+local screngui = Instance.new("ScreenGui")
+screngui.Parent = player:WaitForChild("PlayerGui")
+editlabel.Parent = screngui
+editlabel.PlaceholderText = "Type your chat..."
+editlabel.Size = UDim2.new(0, 512, 0, 60)
+editlabel.Position = UDim2.new(0, 0, 0.5, -25)
+local buttons = Instance.new("TextButton")
+buttons.Text = "Chat!"
+buttons.Position = UDim2.new(0, 0, 0.5, -40)
+buttons.Parent = screnGui
+buttons.MouseButton1Click:Connect(function()
+    local newtext = editlabel.Text
+    textLabel.Text = newtext
+end)
+
+
+
+
