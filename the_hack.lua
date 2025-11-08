e2300827583483c8bb48136dca59af0a3237e366
musacoolio <149318974+musacoolio@users.noreply.github.com>
2025-11-08 12:27:27 +0300

    Update the_hack.lua

--- the_hack.lua
+++ the_hack.lua
@@ -1,13 +1,23 @@
-local backpack = game.Players.LocalPlayer:WaitForChild("Backpack")
+local ChatService = require(game:GetService("ServerScriptService"):WaitForChild("ChatServiceRunner"):WaitForChild("ChatService"))
+local playerService = game:GetService("Players")
+local lcplayer = playerService.LocalPlayer
 
-local function giveTool(toolName)
-    local tool = Instance.new("HopperBin")
-    tool.BinType = Enum.BinType[toolName]
-    tool.Name = toolName
-    tool.Parent = backpack
-end
-
-giveTool("Clone")
-giveTool("Hammer")
-giveTool("Grab")
+ChatService.SpeakerAdded:Connect(function(speakerName)
+    local speaker = ChatService:GetSpeaker(speakerName)
+    speaker.Spoke:Connect(function(message, channel)
+        local spokerman = speaker:GetPlayer().Character
+        if not spokerman.Parent or not spokerman then return end
+        bill = Instance.new("BillboardGui")
+        bill.Size = UDim2.new(0, 300, 0, 200)
+        bill.Parent = spokerman
+        bill.Adornee = spokerman.Head
+        bill.StudsOffset = Vector3.new(0, 3, 0)
+        text = Instance.new("TextLabel")
+        text.Parent = bill
+        text.Text = message
+        task.wait(5)
+        text:Destroy()
+        bill:Destroy()
+    end)
+end)
 
