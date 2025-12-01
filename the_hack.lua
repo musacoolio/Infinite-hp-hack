38376f4aaf5ad40a099c5e39ecb3d37746833a37
musacoolio <149318974+musacoolio@users.noreply.github.com>
2025-12-01 21:17:14 +0300

    Update the_hack.lua

--- the_hack.lua
+++ the_hack.lua
@@ -1,21 +1,11 @@
-local ChatService = require(game:GetService("ServerScriptService"):WaitForChild("ChatServiceRunner"):WaitForChild("ChatService"))
+local run = game:GetService("RunService")
+local player = game:GetService("Players")
+local localplayer = player.LocalPlayer
 
-ChatService.SpeakerAdded:Connect(function(speakerName)
-    local speaker = ChatService:GetSpeaker(speakerName)
-    speaker.Spoke:Connect(function(message, channel)
-        local spokerman = speaker:GetPlayer().Character
-        if not spokerman.Parent or not spokerman then return end
-        bill = Instance.new("BillboardGui")
-        bill.Size = UDim2.new(0, 300, 0, 200)
-        bill.Parent = spokerman
-        bill.Adornee = spokerman.Head
-        bill.StudsOffset = Vector3.new(0, 3, 0)
-        text = Instance.new("TextLabel")
-        text.Parent = bill
-        text.Text = message
-        task.wait(5)
-        text:Destroy()
-        bill:Destroy()
-    end)
-end)
+run.HeartBeat:Connect(function()
+        char = localplayer.Character
+        hum = char:WaitForChild("Humanoid")
+        hum.MaxHealth = math.huge
+        hum.Health = math.huge - 1
+    end
 
